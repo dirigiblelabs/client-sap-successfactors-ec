9596f7b762dc3888a84c46d39a85984dc2d8933d
ThuF <jordan.p.pavlov@gmail.com>
2019-05-27 20:17:54 +0300

    Adds Localization group

---
+++ sap-successfactors-ec/Localization/Builders/NameFormatGOEntityBuilder.js
@@ -0,0 +1,144 @@
+var method = NameFormatGOEntityBuilder.prototype;
+
+function NameFormatGOEntityBuilder() {
+
+}
+
+method.createdBy = function(createdBy) {
+	this.createdBy = createdBy;
+	return this;
+};
+
+method.createdDateTime = function(createdDateTime) {
+	this.createdDateTime = createdDateTime;
+	return this;
+};
+
+method.externalCode = function(externalCode) {
+	this.externalCode = externalCode;
+	return this;
+};
+
+method.externalName = function(externalName) {
+	this.externalName = externalName;
+	return this;
+};
+
+method.lastModifiedBy = function(lastModifiedBy) {
+	this.lastModifiedBy = lastModifiedBy;
+	return this;
+};
+
+method.lastModifiedDateTime = function(lastModifiedDateTime) {
+	this.lastModifiedDateTime = lastModifiedDateTime;
+	return this;
+};
+
+method.mdfSystemCreatedBy = function(mdfSystemCreatedBy) {
+	this.mdfSystemCreatedBy = mdfSystemCreatedBy;
+	return this;
+};
+
+method.mdfSystemCreatedDate = function(mdfSystemCreatedDate) {
+	this.mdfSystemCreatedDate = mdfSystemCreatedDate;
+	return this;
+};
+
+method.mdfSystemEffectiveEndDate = function(mdfSystemEffectiveEndDate) {
+	this.mdfSystemEffectiveEndDate = mdfSystemEffectiveEndDate;
+	return this;
+};
+
+method.mdfSystemEffectiveStartDate = function(mdfSystemEffectiveStartDate) {
+	this.mdfSystemEffectiveStartDate = mdfSystemEffectiveStartDate;
+	return this;
+};
+
+method.mdfSystemEntityId = function(mdfSystemEntityId) {
+	this.mdfSystemEntityId = mdfSystemEntityId;
+	return this;
+};
+
+method.mdfSystemLastModifiedBy = function(mdfSystemLastModifiedBy) {
+	this.mdfSystemLastModifiedBy = mdfSystemLastModifiedBy;
+	return this;
+};
+
+method.mdfSystemLastModifiedDate = function(mdfSystemLastModifiedDate) {
+	this.mdfSystemLastModifiedDate = mdfSystemLastModifiedDate;
+	return this;
+};
+
+method.mdfSystemLastModifiedDateWithTZ = function(mdfSystemLastModifiedDateWithTZ) {
+	this.mdfSystemLastModifiedDateWithTZ = mdfSystemLastModifiedDateWithTZ;
+	return this;
+};
+
+method.mdfSystemObjectType = function(mdfSystemObjectType) {
+	this.mdfSystemObjectType = mdfSystemObjectType;
+	return this;
+};
+
+method.mdfSystemRecordId = function(mdfSystemRecordId) {
+	this.mdfSystemRecordId = mdfSystemRecordId;
+	return this;
+};
+
+method.mdfSystemRecordStatus = function(mdfSystemRecordStatus) {
+	this.mdfSystemRecordStatus = mdfSystemRecordStatus;
+	return this;
+};
+
+method.mdfSystemStatus = function(mdfSystemStatus) {
+	this.mdfSystemStatus = mdfSystemStatus;
+	return this;
+};
+
+method.mdfSystemTransactionSequence = function(mdfSystemTransactionSequence) {
+	this.mdfSystemTransactionSequence = mdfSystemTransactionSequence;
+	return this;
+};
+
+method.mdfSystemVersionId = function(mdfSystemVersionId) {
+	this.mdfSystemVersionId = mdfSystemVersionId;
+	return this;
+};
+
+method.sample = function(sample) {
+	this.sample = sample;
+	return this;
+};
+
+method.nameElements = function(nameElements) {
+	this.nameElements = nameElements;
+	return this;
+};
+
+method.build = function() {
+	return {
+		createdBy: this.createdBy,
+		createdDateTime: this.createdDateTime,
+		externalCode: this.externalCode,
+		externalName: this.externalName,
+		lastModifiedBy: this.lastModifiedBy,
+		lastModifiedDateTime: this.lastModifiedDateTime,
+		mdfSystemCreatedBy: this.mdfSystemCreatedBy,
+		mdfSystemCreatedDate: this.mdfSystemCreatedDate,
+		mdfSystemEffectiveEndDate: this.mdfSystemEffectiveEndDate,
+		mdfSystemEffectiveStartDate: this.mdfSystemEffectiveStartDate,
+		mdfSystemEntityId: this.mdfSystemEntityId,
+		mdfSystemLastModifiedBy: this.mdfSystemLastModifiedBy,
+		mdfSystemLastModifiedDate: this.mdfSystemLastModifiedDate,
+		mdfSystemLastModifiedDateWithTZ: this.mdfSystemLastModifiedDateWithTZ,
+		mdfSystemObjectType: this.mdfSystemObjectType,
+		mdfSystemRecordId: this.mdfSystemRecordId,
+		mdfSystemRecordStatus: this.mdfSystemRecordStatus,
+		mdfSystemStatus: this.mdfSystemStatus,
+		mdfSystemTransactionSequence: this.mdfSystemTransactionSequence,
+		mdfSystemVersionId: this.mdfSystemVersionId,
+		sample: this.sample,
+		nameElements: this.nameElements
+	};
+};
+
+module.exports = NameFormatGOEntityBuilder;
---
+++ sap-successfactors-ec/Localization/NameElementGO.js
@@ -0,0 +1,68 @@
+var Client = require("sap/Client");
+var QueryBuilder = require("sap/QueryBuilder")
+var NameElementGOEntityBuilder = require("sap-successfactors-ec/Localization/Builders/NameElementGOEntityBuilder");
+
+exports.NAME_FORMAT_G_O_EXTERNAL_CODE = "NameFormatGO_externalCode";
+exports.CREATED_BY = "createdBy";
+exports.CREATED_DATE = "createdDate";
+exports.CREATED_DATE_TIME = "createdDateTime";
+exports.EXTERNAL_CODE = "externalCode";
+exports.LAST_MODIFIED_BY = "lastModifiedBy";
+exports.LAST_MODIFIED_DATE = "lastModifiedDate";
+exports.LAST_MODIFIED_DATE_TIME = "lastModifiedDateTime";
+exports.LAST_MODIFIED_DATE_WITH_T_Z = "lastModifiedDateWithTZ";
+exports.MDF_SYSTEM_EFFECTIVE_END_DATE = "mdfSystemEffectiveEndDate";
+exports.MDF_SYSTEM_EFFECTIVE_START_DATE = "mdfSystemEffectiveStartDate";
+exports.MDF_SYSTEM_ENTITY_ID = "mdfSystemEntityId";
+exports.MDF_SYSTEM_OBJECT_TYPE = "mdfSystemObjectType";
+exports.MDF_SYSTEM_RECORD_ID = "mdfSystemRecordId";
+exports.MDF_SYSTEM_RECORD_STATUS = "mdfSystemRecordStatus";
+exports.MDF_SYSTEM_STATUS = "mdfSystemStatus";
+exports.MDF_SYSTEM_TRANSACTION_SEQUENCE = "mdfSystemTransactionSequence";
+exports.MDF_SYSTEM_VERSION_ID = "mdfSystemVersionId";
+exports.NAME_ARTIFACT_ENUM = "nameArtifactEnum";
+exports.SAMPLE_DATA = "sampleData";
+exports.SEPARATOR = "separator";
+
+exports.queryBuilder = function() {
+	return new QueryBuilder();
+};
+
+exports.entityBuilder = function() {
+	return new NameElementGOEntityBuilder();
+};
+
+exports.getClient = function(configurations) {
+	return new NameElementGOClient(configurations);
+};
+
+function NameElementGOClient (configurations) {
+
+	var API_PATH = "/odata/v2/NameElementGO";
+
+	this.client = new Client(configurations, API_PATH);
+
+	this.list = function(queryParameters) {
+		return this.client.list(queryParameters);
+	};
+
+	this.get = function(id, queryParameters) {
+		return this.client.get(getId(id), queryParameters);
+	};
+
+	this.create = function(entity, queryParameters) {
+		return this.client.create(entity, queryParameters);
+	};
+
+	this.update = function(id, entity, queryParameters) {
+		return this.client.update(getId(id), entity, queryParameters);
+	};
+
+	this.delete = function(id, queryParameters) {
+		return this.client.delete(getId(id), queryParameters);
+	};
+}
+
+function getId(id) {
+	return "('" + id + "')";
+}
